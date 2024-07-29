989b8ba52c1cd58ca883a1df37c62b7815f081c3
YurySamsonenka <cant09y@gmail.com>
2024-07-29 22:12:36 +0300

    закончил Подключаем массив Puma

--- src/components/pages/Model.tsx
+++ src/components/pages/Model.tsx
@@ -1,37 +1,45 @@
-import {adidasArr} from "./Adidas";
-import {useParams} from "react-router-dom";
+import { adidasArr, AdidasItem } from './Adidas';
+import { useParams } from 'react-router-dom';
+import { pumaArr, PumaItem } from './Puma';
 
+type CrossModels = {
+	[model: string]: (AdidasItem[] | PumaItem[])
+}
+
+const crossModels: CrossModels = {
+	adidas: adidasArr,
+	puma: pumaArr,
+};
 
 export const Model = () => {
-    const params=useParams()
-     const currentModel=adidasArr.find(el=>el.id===Number(params.id))
+	const { model, id } = useParams();
+	// const currentModel = adidasArr.find(el => el.id === Number(id));
+	const currentModel = model ? crossModels[model].find(el => el.id === Number(id)) : null;
 
-    return (
-        <div style={{textAlign:'center'}}>
-            {currentModel
-                ?<>
-                    <h2>{currentModel.model}</h2>
-                    <h4>{currentModel.collection}</h4>
-                    <h3>{currentModel.price}</h3>
-                    <img
-                        src={currentModel.picture}
-                        alt={currentModel.model}
-                        style={{width: '600px', height: 'auto', marginRight: '10px'}}
-                    />
-                </>
-                :   <h2>Модель отсутствует</h2>
-            }
+	return (
+		<div style={{ textAlign: 'center' }}>
+			{currentModel
+				? <>
+					<h2>{currentModel.model}</h2>
+					<h4>{currentModel.collection}</h4>
+					<h3>{currentModel.price}</h3>
+					<img
+						src={currentModel.picture}
+						alt={currentModel.model}
+						style={{ width: '600px', height: 'auto', marginRight: '10px' }}
+					/>
+				</>
+				: <h2>Модель отсутствует</h2>
+			}
 
-
-
-        {/*    <h2>{adidasArr[Number(params.id)].model}</h2>*/}
-        {/*    <h4>{adidasArr[Number(params.id)].collection}</h4>*/}
-        {/*    <h3>{adidasArr[Number(params.id)].price}</h3>*/}
-        {/*    <img*/}
-        {/*        src={adidasArr[Number(params.id)].picture}*/}
-        {/*        alt={adidasArr[Number(params.id)].model}*/}
-        {/*        style={{width: '600px', height: 'auto', marginRight: '10px'}}*/}
-        {/*    />*/}
-        </div>
-);
+			{/*    <h2>{adidasArr[Number(params.id)].model}</h2>*/}
+			{/*    <h4>{adidasArr[Number(params.id)].collection}</h4>*/}
+			{/*    <h3>{adidasArr[Number(params.id)].price}</h3>*/}
+			{/*    <img*/}
+			{/*        src={adidasArr[Number(params.id)].picture}*/}
+			{/*        alt={adidasArr[Number(params.id)].model}*/}
+			{/*        style={{width: '600px', height: 'auto', marginRight: '10px'}}*/}
+			{/*    />*/}
+		</div>
+	);
 };
